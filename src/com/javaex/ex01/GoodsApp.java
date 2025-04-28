c401694948cd8cbfe8be9506abb44eb8cb6872ec
clz2025-red <clz2025.red@gmail.com>
2025-04-28 16:43:48 +0900

    2025-04-28

---
+++ src/com/javaex/ex01/GoodsApp.java
@@ -0,0 +1,38 @@
+package com.javaex.ex01;
+
+public class GoodsApp {
+
+	//시나리오
+	public static void main(String[] args) {
+		
+		Goods camera = new Goods();
+		camera.name="니콘";
+		camera.price=400000;
+		
+		System.out.println(camera.name);
+		System.out.println(camera.price);
+		
+		System.out.println("----------------");
+		
+		Goods cup = new Goods();
+		cup.name="머그컵";
+		cup.price=2000;
+		
+		System.out.println(cup.name);
+		System.out.println(cup.price);
+		
+		System.out.println("----------------");
+		
+		Goods computer = new Goods();
+		computer.name = "LG그램";
+		computer.price = 1000000;
+		
+		System.out.println(computer.name);
+		System.out.println(computer.price);
+		
+		System.out.println("----------------");
+		System.out.println(cup.name);
+		System.out.println(camera.price);
+	}
+	
+}
